3eca403792e3fa40d3709dd91dac4100fb0c3af3
Glitch (cyan-recess) <none>
2019-03-25 18:49:22 +0000

    05 Project

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Project 04
+   March 25, 18  
+  Jessica Thomas
+*/
+
+var name = "Jess";
+name += " Thomas";
+var nameLength = name.length();
+var shinigami = ["Ichigo", "Gin", "Shunsui"];
+shinigami.push("Kyoraku");
+var substitute = shinigami[0];
+shinigami.shift();
+
